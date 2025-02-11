da339725daf0042016315b3a455ed9e5dc312561
ljy3795517 <130797099+ljy3795517@users.noreply.github.com>
2025-02-11 23:53:01 +0100

    Add files via upload

---
+++ sm.js
@@ -0,0 +1,6 @@
+let data = [
+  ["&&&&&Bonjour", "你好", "bonʒuʁ", "Bonjour, comment allez-vous?"],
+  ["1111Au revoir", "再见", "o ʁəvwaʁ", "Je dois dire au revoir maintenant."],
+  ["Merci", "谢谢", "mɛʁsi", "Merci beaucoup pour votre aide."],
+  ["De rien", "不客气", "də ʁjɛ̃", "De rien, je suis heureux d'aider."]
+];
---
+++ u.js
@@ -0,0 +1,13 @@
+var data = [
+  ["un", "[œ̃]", "一个（阳性单数）", "J'ai un livre   (我有一本书。)"],
+  ["un peu de", "[œ̃ pø də]", "一点点", "Donne-moi un peu de sucre   (给我一点糖。)"],
+  ["une", "[yn]", "一个（阴性单数）", "J'ai une idée   (我有一个主意。)"],
+  ["université", "[y  ni  vɛʁ  si  te]", "大学", "Je vais à l'université   (我去大学。)"],
+  ["universités", "[y  ni  vɛʁ  si  te]", "大学（复数）", "Il y a plusieurs universités ici   (这里有几所大学。)"],
+  ["usine", "[y  zin]", "工厂", "Mon père travaille dans une usine   (我父亲在工厂工作。)"],
+  ["utilise", "[y  ti  liz]", "使用（第一/三人称单数）", "Il utilise un ordinateur   (他使用电脑。)"],
+  ["utiliser", "[y  ti  li  ze]", "使用（动词原形）", "Sais-tu comment utiliser ce logiciel ? (你知道怎么用这个软件吗？)"],
+  ["utilises", "[y  ti  liz]", "使用（第二人称单数）", "Tu utilises ton téléphone ? (你在用手机吗？)"],
+  ["utilisez", "[y  ti  li  ze]", "使用（第二人称复数/敬语）", "Vous utilisez cet outil ? (您使用这个工具吗？)"],
+  ["Utrecht", "[y  tʁɛkt]", "乌得勒支（地名）", "Utrecht est une belle ville   (乌得勒支是个美丽的城市。)"],
+];
